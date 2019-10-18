15e7ea13bff028a53332b9c6ec7dbd162d3c0a06
Guido Perdomo <guidop91@gmail.com>
2019-10-18 18:10:02 -0400

    Update level when inserting conflict in users table

--- sql_queries.py
+++ sql_queries.py
@@ -94,7 +94,8 @@ user_table_insert = ("""
         level
     )
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO NOTHING
+    ON CONFLICT (user_id)
+        DO UPDATE SET level = excluded.level;
 """)
 
 song_table_insert = ("""
